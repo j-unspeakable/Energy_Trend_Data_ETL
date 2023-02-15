eec0ceac41dd59b6889881d930a81a97147976f3
Olojede Joshua <famous.jt33@gmail.com>
2023-02-15 16:37:14 +0100

    fix: Refactor Setup Script

--- setup.py
+++ setup.py
@@ -7,7 +7,13 @@ setup(
     name='energyetl',
     version='1.0',
     author='Louis Esan',
-    description='Description of my package',
-    packages=['package'],
-    install_requires=requirements
+    email="louisgedo@hotmail.com",
+    description="A package to implement a data pipeline for downloading and cleaning energy trend data which is available on the UK government's website",
+    packages=['energyetl'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'energyetl=energyetl.main:main'
+          ]
+    }
 )
