fcb021721978adfba0a2369f3e42a2bcb65ab993
Olojede Joshua <famous.jt33@gmail.com>
2023-02-15 16:38:12 +0100

    fix: Refactor Folder Structure

--- package/save_csv.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def save_data_to_csv(df, filename):
-    # Save the cleaned data as a CSV file.
-    save_filename = os.path.splitext(filename)[0]
-    df.to_csv(f'./data/{save_filename}.csv', index=False)
-
-    return save_filename
